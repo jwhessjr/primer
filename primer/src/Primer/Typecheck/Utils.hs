c38af06bb65838e54716098067dfe302c69c29cc
Ben Price <ben@hackworthltd.com>
2022-10-04 11:59:00 +0100

    pull poly-in-monad out of list. slightly simpler

--- primer/src/Primer/Typecheck/Utils.hs
+++ primer/src/Primer/Typecheck/Utils.hs
@@ -106,25 +106,26 @@ instantiateValCons t = do
           . Compose
   -- We eta-expand here to deal with simplified subsumption
   {- HLINT ignore instantiateValCons "Use id" -}
-  fmap (fmap reassoc') $ sequence4 $ fmap (second (fmap $ second $ fmap $ \x -> x) . reassoc) instCons
+  fmap (fmap reassoc') $ sequence4 $ fmap (fmap (fmap $ fmap $ \x -> x) . reassoc) instCons
 
 -- | As 'instantiateValCons', but pulls out the relevant bits of the monadic
 -- context into an argument
 instantiateValCons' ::
   TypeDefMap ->
   Type' () ->
-  Either TypeDefError (TyConName, ASTTypeDef, [(ValConName, [forall m. MonadFresh NameCounter m => m (Type' ())])])
+  Either TypeDefError (TyConName, ASTTypeDef, [(ValConName, forall m. MonadFresh NameCounter m =>  [m (Type' ())])])
 instantiateValCons' tyDefs t =
   getTypeDefInfo' tyDefs t
     >>= \(TypeDefInfo params tc def) -> case def of
       TypeDefPrim _ -> Left TDINotADT
       TypeDefAST tda -> do
         let defparams = map fst $ astTypeDefParameters tda
-            f :: ValCon -> (ValConName, [forall m. MonadFresh NameCounter m => m (Type' ())])
+            f :: ValCon -> (ValConName, forall m. MonadFresh NameCounter m => [m (Type' ())])
             -- eta expand to deal with shallow subsumption
             {- HLINT ignore instantiateValCons' "Avoid lambda" -}
             f c = (valConName c, map (\a -> substituteTypeVars (zip defparams params) a) $ valConArgs c)
         pure (tc, tda, map f $ astTypeDefConstructors tda)
+
 
 substituteTypeVars :: MonadFresh NameCounter m => [(TyVarName, Type' ())] -> Type' () -> m (Type' ())
 substituteTypeVars = flip $ foldrM (uncurry substTy)
