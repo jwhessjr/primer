d38a3c4a7f2d83304661cc36771129c677bab6ba
Ben Price <ben@hackworthltd.com>
2023-05-12 01:36:58 +0100

    trim Def.Utils

--- primer/src/Primer/Def/Utils.hs
+++ primer/src/Primer/Def/Utils.hs
@@ -1,26 +1,14 @@
 {-# LANGUAGE OverloadedLabels #-}
 
-module Primer.Def.Utils (nextID, globalInUse) where
+module Primer.Def.Utils (globalInUse) where
 
 import Foreword
 
 import Data.Set qualified as Set
-import Optics (anyOf, folded, foldlOf', to, (%))
-import Primer.Core.Meta (GVarName, ID)
-import Primer.Core.Utils (exprIDs, freeGlobalVars, typeIDs)
+import Optics (anyOf, folded, to, (%))
+import Primer.Core.Meta (GVarName)
+import Primer.Core.Utils (freeGlobalVars)
 import Primer.Def (ASTDef (..), Def (..))
-
--- | Given a 'Def', return its next 'ID'.
---
--- Note: do not rely on the implementation of this function, as it may
--- change in the future.
-nextID :: Def -> ID
-nextID (DefAST (ASTDef e t)) =
-  let eid = foldlOf' exprIDs max minBound e
-      tid = foldlOf' typeIDs max minBound t
-   in succ $ max eid tid
-nextID (DefPrim _) = 0
-{-# INLINE nextID #-}
 
 globalInUse :: Foldable f => GVarName -> f Def -> Bool
 globalInUse v =
