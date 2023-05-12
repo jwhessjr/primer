a6ad1257e639ffd9aea0e91ac9535df6cd5b5eef
Ben Price <ben@hackworthltd.com>
2023-05-12 01:38:27 +0100

    trim Log

--- primer/src/Primer/Log.hs
+++ primer/src/Primer/Log.hs
@@ -1,13 +1,8 @@
 module Primer.Log (
   ConvertLogMessage (..),
-  logCritical,
   logDebug,
-  logError,
   logEmergency,
-  logInfo,
-  logNotice,
   logWarning,
-  textWithSeverity,
   PureLogT,
   runPureLogT,
   PureLog,
@@ -26,7 +21,6 @@ import Control.Monad.Log (
   LoggingT,
   MonadLog,
   PureLoggingT,
-  Severity (..),
   WithSeverity (..),
   discardLogging,
   logMessage,
@@ -34,31 +28,14 @@ import Control.Monad.Log (
   runPureLoggingT,
  )
 import Control.Monad.Log qualified as Log (
-  logCritical,
   logDebug,
   logEmergency,
-  logError,
-  logInfo,
-  logNotice,
   logWarning,
  )
 import Control.Monad.Trans (MonadTrans)
 
-logSeverity :: Severity -> Text
-logSeverity Debug = "[DEBUG]     "
-logSeverity Informational = "[INFO]      "
-logSeverity Notice = "[NOTICE]    "
-logSeverity Warning = "[WARNING]   "
-logSeverity Error = "[ERROR]     "
-logSeverity Critical = "[CRITICAL]  "
-logSeverity Alert = "[ALERT]     "
-logSeverity Emergency = "[EMERGENCY] "
-
 class ConvertLogMessage source target where
   convert :: source -> target
-
-logCritical :: (ConvertLogMessage source target, MonadLog (WithSeverity target) m) => source -> m ()
-logCritical = Log.logCritical . convert
 
 logDebug :: (ConvertLogMessage source target, MonadLog (WithSeverity target) m) => source -> m ()
 logDebug = Log.logDebug . convert
@@ -66,20 +43,8 @@ logDebug = Log.logDebug . convert
 logEmergency :: (ConvertLogMessage source target, MonadLog (WithSeverity target) m) => source -> m ()
 logEmergency = Log.logEmergency . convert
 
-logError :: (ConvertLogMessage source target, MonadLog (WithSeverity target) m) => source -> m ()
-logError = Log.logError . convert
-
-logInfo :: (ConvertLogMessage source target, MonadLog (WithSeverity target) m) => source -> m ()
-logInfo = Log.logInfo . convert
-
-logNotice :: (ConvertLogMessage source target, MonadLog (WithSeverity target) m) => source -> m ()
-logNotice = Log.logNotice . convert
-
 logWarning :: (ConvertLogMessage source target, MonadLog (WithSeverity target) m) => source -> m ()
 logWarning = Log.logWarning . convert
-
-textWithSeverity :: WithSeverity Text -> Text
-textWithSeverity (WithSeverity s m) = logSeverity s <> m
 
 -- | Convenient for discarding logging.
 instance ConvertLogMessage a () where
