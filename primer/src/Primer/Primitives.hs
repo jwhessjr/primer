e90cf919b9a8396326fc6f57a71926d25664ea0a
Ben Price <ben@hackworthltd.com>
2023-05-12 01:22:24 +0100

    trim Primitives

--- primer/src/Primer/Primitives.hs
+++ primer/src/Primer/Primitives.hs
@@ -7,52 +7,32 @@ module Primer.Primitives (
   tInt,
   tChar,
   primitive,
-  primitiveGVar,
   primConName,
   primDefName,
   primDefType,
-  primFunDef,
   PrimFunError (..),
   primitiveModuleName,
 ) where
 
 import Foreword
 
-import Control.Monad.Fresh (MonadFresh)
 import Data.Aeson (FromJSON (..), ToJSON (..))
 import Data.Data (Data)
 import Data.Map qualified as M
-import Numeric.Natural (Natural)
 import Primer.Builtins (
-  cJust,
-  cNothing,
-  cSucc,
-  cZero,
   tBool,
   tMaybe,
   tNat,
  )
-import Primer.Builtins.DSL (bool_, maybe_, nat)
 import Primer.Core (
-  Expr,
-  Expr' (App, Con, PrimCon),
-  GVarName,
+  Expr',
   GlobalName,
-  ID,
   ModuleName,
   PrimCon (PrimChar, PrimInt),
   TyConName,
   Type' (..),
   mkSimpleModuleName,
   qualifyName,
- )
-import Primer.Core.DSL (
-  aPP,
-  app,
-  char,
-  con,
-  int,
-  tcon,
  )
 import Primer.JSON (CustomJSON (..), PrimerJSON)
 import Primer.Name (Name)
@@ -86,10 +66,6 @@ tChar = primitive "Char"
 
 tInt :: TyConName
 tInt = primitive "Int"
-
--- | Construct a reference to a primitive definition.
-primitiveGVar :: PrimDef -> GVarName
-primitiveGVar = primitive . primDefName
 
 -- | Primitive type definitions.
 -- There should be one entry here for each constructor of `PrimCon`.
@@ -169,121 +145,3 @@ primFunTypes = \case
   where
     c = TCon ()
     a = TApp ()
-
-primFunDef :: PrimDef -> [Expr' () ()] -> Either PrimFunError (forall m. MonadFresh ID m => m Expr)
-primFunDef def args = case def of
-  ToUpper -> case args of
-    [PrimCon _ (PrimChar c)] ->
-      Right $ char $ toUpper c
-    _ -> err
-  IsSpace -> case args of
-    [PrimCon _ (PrimChar c)] ->
-      Right $ bool_ $ isSpace c
-    _ -> err
-  HexToNat -> case args of
-    [PrimCon _ (PrimChar c)] -> Right $ maybe_ (tcon tNat) nat $ digitToIntSafe c
-      where
-        digitToIntSafe :: Char -> Maybe Natural
-        digitToIntSafe c' = fromIntegral <$> (guard (isHexDigit c') $> digitToInt c')
-    _ -> err
-  NatToHex -> case args of
-    [exprToNat -> Just n] ->
-      Right $ maybe_ (tcon tChar) char $ intToDigitSafe n
-      where
-        intToDigitSafe :: Natural -> Maybe Char
-        intToDigitSafe n' = guard (0 <= n && n <= 15) $> intToDigit (fromIntegral n')
-    _ -> err
-  EqChar -> case args of
-    [PrimCon _ (PrimChar c1), PrimCon _ (PrimChar c2)] ->
-      Right $ bool_ $ c1 == c2
-    _ -> err
-  IntAdd -> case args of
-    [PrimCon _ (PrimInt x), PrimCon _ (PrimInt y)] ->
-      Right $ int $ x + y
-    _ -> err
-  IntMinus -> case args of
-    [PrimCon _ (PrimInt x), PrimCon _ (PrimInt y)] ->
-      Right $ int $ x - y
-    _ -> err
-  IntMul -> case args of
-    [PrimCon _ (PrimInt x), PrimCon _ (PrimInt y)] ->
-      Right $ int $ x * y
-    _ -> err
-  IntQuotient -> case args of
-    [PrimCon _ (PrimInt x), PrimCon _ (PrimInt y)] ->
-      Right $
-        if y == 0
-          then con cNothing `aPP` tcon tInt
-          else
-            con cJust
-              `aPP` tcon tInt
-              `app` int (x `div` y)
-    _ -> err
-  IntRemainder -> case args of
-    [PrimCon _ (PrimInt x), PrimCon _ (PrimInt y)] ->
-      Right $
-        if y == 0
-          then con cNothing `aPP` tcon tInt
-          else
-            con cJust
-              `aPP` tcon tInt
-              `app` int (x `mod` y)
-    _ -> err
-  IntQuot -> case args of
-    [PrimCon _ (PrimInt x), PrimCon _ (PrimInt y)] ->
-      Right $
-        int $
-          if y == 0 then 0 else x `div` y
-    _ -> err
-  IntRem -> case args of
-    [PrimCon _ (PrimInt x), PrimCon _ (PrimInt y)] ->
-      Right $
-        int $
-          if y == 0
-            then x
-            else x `mod` y
-    _ -> err
-  IntLT -> case args of
-    [PrimCon _ (PrimInt x), PrimCon _ (PrimInt y)] ->
-      Right $ bool_ $ x < y
-    _ -> err
-  IntLTE -> case args of
-    [PrimCon _ (PrimInt x), PrimCon _ (PrimInt y)] ->
-      Right $ bool_ $ x <= y
-    _ -> err
-  IntGT -> case args of
-    [PrimCon _ (PrimInt x), PrimCon _ (PrimInt y)] ->
-      Right $ bool_ $ x > y
-    _ -> err
-  IntGTE -> case args of
-    [PrimCon _ (PrimInt x), PrimCon _ (PrimInt y)] ->
-      Right $ bool_ $ x >= y
-    _ -> err
-  IntEq -> case args of
-    [PrimCon _ (PrimInt x), PrimCon _ (PrimInt y)] ->
-      Right $ bool_ $ x == y
-    _ -> err
-  IntNeq -> case args of
-    [PrimCon _ (PrimInt x), PrimCon _ (PrimInt y)] ->
-      Right $ bool_ $ x /= y
-    _ -> err
-  IntToNat -> case args of
-    [PrimCon _ (PrimInt x)] ->
-      Right $
-        if x < 0
-          then con cNothing `aPP` tcon tNat
-          else
-            con cJust
-              `aPP` tcon tNat
-              `app` nat (fromInteger x)
-    _ -> err
-  IntFromNat -> case args of
-    [exprToNat -> Just n] ->
-      Right $ int $ fromIntegral n
-    _ -> err
-  where
-    exprToNat = \case
-      Con _ c | c == cZero -> Just 0
-      App _ (Con _ c) x | c == cSucc -> succ <$> exprToNat x
-      _ -> Nothing
-    err = Left $ PrimFunError def args
