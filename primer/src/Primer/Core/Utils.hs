5f35b285e260b5fe64584194ab1076c3afbd40b3
Ben Price <ben@hackworthltd.com>
2023-05-13 19:07:23 +0100

    merge Core.Type.Utils and Core.Fresh into Core.Utils

--- primer/src/Primer/Core/Utils.hs
+++ primer/src/Primer/Core/Utils.hs
@@ -17,6 +17,8 @@ module Primer.Core.Utils (
 import Foreword
 
 import Control.Monad.Fresh (MonadFresh, fresh)
+import Data.Data (Data)
+import Data.Generics.Uniplate.Data (universe)
 import Data.Set qualified as S
 import Optics (
   Fold,
@@ -31,31 +33,78 @@ import Optics (
   traverseOf,
   (%),
  )
-
 import Primer.Core (
   CaseBranch' (..),
   Expr' (..),
   GVarName,
   HasID (_id),
   ID,
+  LocalName (LocalName),
   LVarName,
   TyVarName,
   Type' (..),
   bindName,
   _exprMeta,
   _exprTypeMeta,
+  _typeMeta,
  )
-import Primer.Core.Fresh (freshLocalName')
-import Primer.Core.Type.Utils (
-  alphaEqTy,
-  forgetTypeMetadata,
-  noHoles,
-  regenerateTypeIDs,
-  traverseFreeVarsTy,
-  typeIDs,
-  _freeVarsTy,
- )
+import Primer.Name (Name, NameCounter, freshName)
 
+-- | Helper, wrapping 'freshName'
+freshLocalName' :: MonadFresh NameCounter m => S.Set Name -> m (LocalName k)
+freshLocalName' = fmap LocalName . freshName
+
+-- | Regenerate all IDs, not changing any other metadata
+regenerateTypeIDs :: (HasID a, MonadFresh ID m) => Type' a -> m (Type' a)
+regenerateTypeIDs = regenerateTypeIDs' (set _id)
+
+regenerateTypeIDs' :: MonadFresh ID m => (ID -> a -> b) -> Type' a -> m (Type' b)
+regenerateTypeIDs' s = traverseOf _typeMeta (\a -> flip s a <$> fresh)
+
+-- | Replace all 'ID's in a Type with unit.
+-- Technically this replaces all annotations, regardless of what they are.
+forgetTypeMetadata :: Type' a -> Type' ()
+forgetTypeMetadata = set _typeMeta ()
+
+-- | Test whether an type contains any holes
+-- (empty or non-empty, or inside a kind)
+noHoles :: Data a => Type' a -> Bool
+noHoles t = flip all (universe t) $ \case
+  TEmptyHole{} -> False
+  _ -> True
+
+_freeVarsTy :: Traversal (Type' a) (Type' a) (a, TyVarName) (Type' a)
+_freeVarsTy = traversalVL $ traverseFreeVarsTy mempty
+
+-- Helper for _freeVarsTy and _freeTyVars
+-- Takes a set of considered-to-be-bound variables
+traverseFreeVarsTy :: Applicative f => Set TyVarName -> ((a, TyVarName) -> f (Type' a)) -> Type' a -> f (Type' a)
+traverseFreeVarsTy = go
+  where
+    go bound f = \case
+      t@TEmptyHole{} -> pure t
+      t@TCon{} -> pure t
+      TFun m s t -> TFun m <$> go bound f s <*> go bound f t
+
+-- Check two types for alpha equality
+--
+-- it makes usage easier if this is pure
+-- i.e. we don't want to need a fresh name supply
+-- We assume both inputs are both from the same context
+--
+-- Note that we do not expand TLets, they must be structurally
+-- the same (perhaps with a different named binding)
+alphaEqTy :: Type' () -> Type' () -> Bool
+alphaEqTy = go
+  where
+    go (TEmptyHole _) (TEmptyHole _) = True
+    go (TCon _ n) (TCon _ m) = n == m
+    go  (TFun _ a b) (TFun _ c d) = go a c && go b d
+    go _ _ = False
+
+-- | Traverse the 'ID's in a 'Type''.
+typeIDs :: HasID a => Traversal' (Type' a) ID
+typeIDs = _typeMeta % _id
 -- | Regenerate all IDs, not changing any other metadata
 regenerateExprIDs :: (HasID a, HasID b, MonadFresh ID m) => Expr' a b -> m (Expr' a b)
 regenerateExprIDs = regenerateExprIDs' (set _id) (set _id)
