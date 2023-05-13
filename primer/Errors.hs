66e01cb183abc5bc2e332e4ec4b07da9baa81ebf
Ben Price <ben@hackworthltd.com>
2023-05-14 00:30:41 +0100

    unused errors

--- primer/Errors.hs
+++ primer/Errors.hs
@@ -12,40 +12,17 @@ module Errors (
 import Foreword
 
 import Actions (Action)
-import Core (Expr)
-import Movement (Movement)
-import Type (Kind, Type')
 
 data Error
   = UnknownTypeConstructor Text
-  | InconsistentKinds Kind Kind
-  | KindDoesNotMatchArrow Kind
   | InternalError Text
-  | CannotSynthesiseType Expr
-  | InconsistentTypes (Type' ()) (Type' ())
-  | TypeDoesNotMatchArrow (Type' ())
-  | CustomFailure
-      Action
-      -- ^ action that caused the error
-      Text
-      -- ^ the error message
+  | CustomFailure Action Text
   | InternalFailure Text
   | IDNotFound Int
-  | MovementFailed (Int, Movement)
-  | -- | Both actual and potential, eg renaming the lambda x to y in any of
-    -- λx.y     the binder captures the existing y
-    -- λx.λy.x  occurance gets captured by the inner binder
-    -- λx.λy.y  this would be ok, but we are paranoid and bail out
-    NameCapture
+  | NameCapture
   | NoNodeSelection
   | NoDefSelected
   | DefNotFound Text
   | DefAlreadyExists Text
   | DefInUse Text
-  | -- | Currently copy/paste is only exposed in the frontend via select
-    --   channels, which should never go wrong. Consequently, this is an
-    --   "internal error" which should never happen!
-    --   If/when we expose it more broadly, we should refactor this to contain
-    --   a descriptive ADT, rather than a string.
-    CopyPasteError Text
   deriving stock (Eq, Show, Read)
