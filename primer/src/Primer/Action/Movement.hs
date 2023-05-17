9c2f2541465ce360992e821f15baffcb8b8fa015
Ben Price <ben@hackworthltd.com>
2023-05-17 20:15:50 +0100

    remove dead modules: Action*, App*, Questions, Gen/App, Tests/Typecheck

--- primer/src/Primer/Action/Movement.hs
+++
@@ -1,9 +0,0 @@
-module Primer.Action.Movement (Movement (..)) where
-
-import Foreword
-
-import Primer.Core.Meta (ValConName)
-
--- | Core movements
-data Movement = Child1 | Child2 | Parent | Branch ValConName
-  deriving stock (Eq, Show, Read)
