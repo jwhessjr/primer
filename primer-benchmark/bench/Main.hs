3beb956c57accf10aa8bd8eee5c235692db3c646
Ben Price <ben@hackworthltd.com>
2023-05-17 20:17:23 +0100

    remove dead packages

--- primer-benchmark/bench/Main.hs
+++
@@ -1,9 +0,0 @@
-module Main (main) where
-
-import Foreword
-
-import Benchmarks (benchmarks, runBenchmarks)
-import Criterion.Main (defaultMain)
-
-main :: IO ()
-main = defaultMain $ runBenchmarks benchmarks
--- primer-benchmark/test/Main.hs
+++
@@ -1,9 +0,0 @@
-module Main (main) where
-
-import Foreword
-
-import Benchmarks (benchmarks, runTests)
-import Test.Tasty (defaultMain)
-
-main :: IO ()
-main = defaultMain $ runTests benchmarks
