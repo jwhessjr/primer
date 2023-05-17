3f61b491ac70415d926c2af6165c4887ac387e50
Ben Price <ben@hackworthltd.com>
2023-05-17 20:44:22 +0100

    weed Zipper.Type

--- primer/src/Primer/Zipper/Type.hs
+++ primer/src/Primer/Zipper/Type.hs
@@ -3,30 +3,15 @@
 -- | This module contains the zipper type @TypeZ@, and functions for
 --  operating on them.
 module Primer.Zipper.Type (
-  TypeZip,
-  IsZipper (asZipper),
   focus,
   target,
-  _target,
-  replace,
-  focusOnTy,
-  top,
-  up,
   down,
   left,
   right,
   farthest,
-  search,
-  FoldAbove,
-  FoldAbove' (FA, current, prior),
-  foldAbove,
   foldBelow,
-  bindersAboveTy,
-  LetTypeBinding' (LetTypeBind),
-  LetTypeBinding,
   getBoundHereTy',
   getBoundHereTy,
-  getBoundHereUpTy,
   getBoundHereDnTy,
   bindersBelowTy,
 ) where
@@ -37,32 +22,22 @@ import Data.Data (Data)
 import Data.Generics.Uniplate.Data ()
 import Data.Generics.Uniplate.Zipper (
   Zipper,
-  replaceHole,
   zipper,
  )
 import Data.Generics.Uniplate.Zipper qualified as Z
 import Data.Set qualified as S
 import Optics (
-  over,
   view,
  )
-import Optics.Lens (Lens', equality', lens)
+import Optics.Lens (Lens', equality')
 import Optics.Traversal (traverseOf)
 import Primer.Core.Meta (
-  HasID (..),
-  ID,
   TyVarName,
-  getID,
  )
 import Primer.Core.Type (
   Type' (TForall, TLet),
-  TypeMeta,
  )
 
-type TypeZip' b = Zipper (Type' b) (Type' b)
-
--- | An ordinary zipper for 'Type's
-type TypeZip = TypeZip' TypeMeta
 
 -- | We want to use up, down, left, right, etc. on 'ExprZ' and 'TypeZ',
 -- despite them being very different types. This class enables that, by proxying
@@ -78,13 +53,6 @@ instance Data a => IsZipper (Z.Zipper a a) a where
 target :: IsZipper za a => za -> a
 target = Z.hole . view asZipper
 
--- | A 'Lens' for the target of a zipper
-_target :: IsZipper za a => Lens' za a
-_target = lens target (flip replace)
-
-up :: IsZipper za a => za -> Maybe za
-up = traverseOf asZipper Z.up
-
 down :: (IsZipper za a) => za -> Maybe za
 down = traverseOf asZipper Z.down
 
@@ -94,69 +62,13 @@ left = traverseOf asZipper Z.left
 right :: IsZipper za a => za -> Maybe za
 right = traverseOf asZipper Z.right
 
-top :: IsZipper za a => za -> za
-top = farthest up
-
 -- | Convert a normal 'Expr' or 'Type' to a cursored one, focusing on the root
 focus :: (Data a) => a -> Zipper a a
 focus = zipper
 
--- | Replace the node at the cursor with the given value.
-replace :: (IsZipper za a) => a -> za -> za
-replace = over asZipper . replaceHole
-
--- | Focus on the node with the given 'ID', if it exists in the type
-focusOnTy ::
-  (Data b, HasID b) =>
-  ID ->
-  Type' b ->
-  Maybe (Zipper (Type' b) (Type' b))
-focusOnTy i = focusOnTy' i . focus
-
--- | Focus on the node with the given 'ID', if it exists in the focussed type
-focusOnTy' ::
-  (Data b, HasID b) =>
-  ID ->
-  Zipper (Type' b) (Type' b) ->
-  Maybe (Zipper (Type' b) (Type' b))
-focusOnTy' i = fmap snd . search matchesID
-  where
-    matchesID z
-      -- If the current target has the correct ID, return that
-      | getID (target z) == i = Just z
-      | otherwise = Nothing
-
--- | Search for a node for which @f@ returns @Just@ something.
--- Performs a depth-first, leftmost-first search.
-search :: (IsZipper za a) => (za -> Maybe b) -> za -> Maybe (za, b)
-search f z
-  | Just x <- f z = Just (z, x)
-  | otherwise =
-      -- if the node has children, recurse on the leftmost child
-      (down z >>= search f . farthest left)
-        -- then recurse on the sibling to the right
-        <|> (right z >>= search f)
-
 -- | Move the zipper focus as far in one direction as possible
 farthest :: (a -> Maybe a) -> a -> a
 farthest f = go where go a = maybe a go (f a)
-
-data FoldAbove' a b = FA {prior :: a, current :: b}
-type FoldAbove a = FoldAbove' a a
-
--- | Focus on everything 'up', in order, map each to a monoid, and accumulate.
--- This does not focus on the current target.
--- We keep track of which child we just came up from, as that can be important
--- in applications: e.g. finding enclosing binders, where @let x=e1 in e2@ has
--- two children, but only binds a variable in one of them.
--- NB: 'foldAbove' + 'foldBelow' does not encompass the whole term: it misses
--- siblings.
-foldAbove :: (IsZipper za a, Monoid m) => (FoldAbove a -> m) -> za -> m
-foldAbove f z = go (target z) (up z)
-  where
-    go p c = case c of
-      Nothing -> mempty
-      Just z' -> let cur = target z' in f (FA{prior = p, current = cur}) <> go cur (up z')
 
 -- | Focus on the current thing, and then everything 'below', in depth-first,
 -- leftmost-first order;
@@ -168,18 +80,11 @@ foldBelow f z = f (target z) <> maybe mempty (go . farthest left) (down z)
   where
     go z' = f (target z') <> maybe mempty (go . farthest left) (down z') <> maybe mempty go (right z')
 
-bindersAboveTy :: TypeZip -> S.Set TyVarName
-bindersAboveTy = foldAbove getBoundHereUpTy
-
 -- Note that we have two specialisations we care about:
 -- bindersBelowTy :: TypeZip -> S.Set Name
 -- bindersBelowTy :: Zipper (Type' One) (Type' One) -> S.Set Name
 bindersBelowTy :: (Data a, Eq a) => Zipper (Type' a) (Type' a) -> S.Set TyVarName
 bindersBelowTy = foldBelow getBoundHereDnTy
-
--- Get the names bound by this layer of an type for a given child.
-getBoundHereUpTy :: Eq a => FoldAbove (Type' a) -> S.Set TyVarName
-getBoundHereUpTy e = getBoundHereTy (current e) (Just $ prior e)
 
 -- Get all names bound by this layer of an type, for any child.
 getBoundHereDnTy :: Eq a => Type' a -> S.Set TyVarName
@@ -190,7 +95,6 @@ getBoundHereTy t prev = S.fromList $ either identity (\(LetTypeBind n _) -> n) <
 
 data LetTypeBinding' a = LetTypeBind TyVarName (Type' a)
   deriving stock (Eq, Show)
-type LetTypeBinding = LetTypeBinding' TypeMeta
 
 getBoundHereTy' :: Eq a => Type' a -> Maybe (Type' a) -> [Either TyVarName (LetTypeBinding' a)]
 getBoundHereTy' t prev = case t of
