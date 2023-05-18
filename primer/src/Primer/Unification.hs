635a93615071f85c6ff5c1ed30200ad832931629
Ben Price <ben@hackworthltd.com>
2023-05-18 13:59:30 +0100

    simplify return type of unify

--- primer/src/Primer/Unification.hs
+++ primer/src/Primer/Unification.hs
@@ -2,10 +2,6 @@ module Primer.Unification (unify) where
 
 import Foreword
 
-import Data.Map qualified as M
-import Primer.Core.Meta (
-  TyVarName,
- )
 import Primer.Core.Type (
   Type' (TApp, TEmptyHole, TFun),
  )
@@ -13,48 +9,11 @@ import Primer.Typecheck.Kindcheck (
   Type,
  )
 
--- | Attempts to find a substitution for the given variables that makes the types consistent (i.e. equal-up-to-holes).
--- We represent unification variables as TVars which happen to have names in the given set.
--- We unify without caring about kinds, but afterwards check that the solution is well-kinded.
--- We ensure that (if a unifier is found) the returned substitution is idempotent, in the sense that
--- it only needs to be applied once to remove all solved unification variables; i.e. in the RHS there
--- never appears a solved unification variable.
--- We ensure we are stable under swapping the two input types
---  (for testing purposes: it is easy to do here, but quite awkward to figure out what the symmetry property should be if we don't
---   - the problem starts when unifying two unifvars: could get either way around, but then the knockon effects are complex
---   - eg @(unif1 -> unif3) \`unify\` (unif2 -> List unif2)@ says @unif1:=unif2; unif3:=List unif2@
---   - but symmetric version might say @unif2:=unif1; unif3:=List unif1@
---   - We are careful to always give the first result.
---   - However, this choice is not stable under renaming unification variables: an equivalent
---     unification problem with different names may choose the "other" solution.
---  )
-unify ::
-  Type ->
-  Type ->
-  Maybe (M.Map TyVarName Type)
-unify s t = do
-  let result = unU $ unify' s t
-  case result of
-    Left _err -> Nothing
-    Right _ -> Just mempty
-
-data UnifError
-  = NotUnify Type Type
-
-newtype U a = U {unU :: Either UnifError a}
-  deriving newtype
-    ( Functor
-    , Applicative
-    , Monad
-    , MonadError UnifError
-    )
-
--- We assume (both empty and non-empty) holes can unify with anything!
-unify' :: Type -> Type -> U ()
-unify' (TEmptyHole _) _ = pure ()
-unify' _ (TEmptyHole _) = pure ()
-unify' (TFun _ s1 t1) (TFun _ s2 t2) = unify' s1 s2 >> unify' t1 t2
+unify :: Type -> Type -> Maybe ()
+unify (TEmptyHole _) _ = pure ()
+unify _ (TEmptyHole _) = pure ()
+unify (TFun _ s1 t1) (TFun _ s2 t2) = unify s1 s2 >> unify t1 t2
 -- Doing first-order unification, as applications are only constructor-like
 -- (we don't have any bona fide functions at the type level)
-unify' (TApp _ s1 t1) (TApp _ s2 t2) = unify' s1 s2 >> unify' t1 t2
-unify' s t = throwError $ NotUnify s t
+unify (TApp _ s1 t1) (TApp _ s2 t2) = unify s1 s2 >> unify t1 t2
+unify _ _ = Nothing
