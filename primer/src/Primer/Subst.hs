e4b4619ccf4013d8a90d5b3301e158e232595df6
Ben Price <ben@hackworthltd.com>
2023-05-18 14:10:13 +0100

    subst is trivial

--- primer/src/Primer/Subst.hs
+++ primer/src/Primer/Subst.hs
@@ -4,22 +4,12 @@ module Primer.Subst (
 
 import Foreword
 
-import Control.Monad.Fresh (MonadFresh)
-import Data.Map qualified as M
 import Primer.Core.Meta (TyVarName)
 import Primer.Core.Type (Type' (..))
-import Primer.Name (NameCounter)
 
 -- | Simple and inefficient capture-avoiding simultaneous substitution.
 -- @substTySimul [(a,A),(b,B)] t@ is @t[A,B/a,b]@, where any references to @a,b@
 -- in their replacements @A,B@ are not substituted.
 -- We restrict to '()', i.e. no metadata as we don't want to duplicate IDs etc
-substTySimul :: MonadFresh NameCounter m => Map TyVarName (Type' ()) -> Type' () -> m (Type' ())
-substTySimul sub
-  | M.null sub = pure
-  | otherwise = go
-  where
-    go = \case
-      t@TEmptyHole{} -> pure t
-      TFun _ s t -> TFun () <$> go s <*> go t
-      TApp _ s t -> TApp () <$> go s <*> go t
+substTySimul :: Applicative m => Map TyVarName (Type' ()) -> Type' () -> m (Type' ())
+substTySimul _sub = pure
